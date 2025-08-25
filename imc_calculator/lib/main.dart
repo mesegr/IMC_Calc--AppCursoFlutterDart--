160b9047ec3112bef6b905d0de7e25d424272675
Alejandro Yañez Meseguer <aymeseguer@gmail.com>
2025-08-25 13:25:59 +0200

    background y appbar

--- imc_calculator/lib/main.dart
+++ imc_calculator/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:imc_calculator/core/app_colors.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,8 +10,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
+        appBar: AppBar(title: Text('Imc Calculator'),
+                        backgroundColor: AppColors.primary,
+                        foregroundColor: Colors.white,),
+        backgroundColor: AppColors.background,
         body: Center(
           child: Text('Hello World!'),
         ),
